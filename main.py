5c02a1313d9998885c564bd1346db50bf431d492
pandy <yangpandy@gmail.com>
2022-08-09 09:44:52 +0800

    fix: 进程异常退出 需要释放进程锁

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 import sys, os, signal
+
 sys.path.append(os.path.dirname(__file__) + os.sep + '../')
 from multiprocessing import Process
 import time
@@ -11,12 +12,14 @@ import multiprocessing
 # 进程锁
 proc_lock = multiprocessing.Lock()
 
+
 class Item:
     def __init__(self, target, name):
         self.target = target
         self.name = name
         self.process = None
         self.start_time = 0
+
 
 def main():
     processes = []
@@ -27,7 +30,7 @@ def main():
     while True:
         for p in processes:
             if p.process is None:
-                p.process = Process(target=p.target, name=p.name, daemon=False, args=(proc_lock, ))
+                p.process = Process(target=p.target, name=p.name, daemon=False, args=(proc_lock,))
                 p.process.start()
                 print(f'启动{p.name}进程，pid={p.process.pid}')
                 p.start_time = time.time()
@@ -39,12 +42,19 @@ def main():
                     p.process.terminate()
                     p.process = None
 
+                    try:
+                        proc_lock.release()
+                    except ValueError as e:
+                        print(str(e))
+                        pass
+
                 # elif p.start_time + 60 * 60 < time.time(): # 最长运行1小时就重启
                 #     print(f'进程{p.name}运行太久，重启')
                 #     p.process.terminate()
                 #     p.process = None
 
         time.sleep(0.2)
+
 
 def citest():
     """
@@ -68,6 +78,7 @@ def citest():
         assert p.process is not None
         assert p.process.is_alive()
         p.process.terminate()
+
 
 if __name__ == '__main__':
     try:
